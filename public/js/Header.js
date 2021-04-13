4c06582e5207afe8574bc9df1bcc186f0ebe36ad
denuradhan <denny.dand@outlook.com>
2021-04-13 20:55:10 +0700

    push kabe

---
+++ public/js/Header.js
@@ -0,0 +1,6 @@
+function seacrchFunction(){
+    var x = document.getElementById("inputSearch").value;
+    if(x != null){
+        document.getElementById("demo").innerHTML = x;
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Body from './components/Body';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header handleClick={handleClick}/>
+      <Body rename={rename}/>
+      <Footer/>
     </div>
   );
+}
+function handleClick() {
+  var x = document.getElementById("inputSearch").value
+  if(x != null){
+      console.log(x)
+      rename(x)
+  }
+}
+
+function rename(string){
+  document.getElementById("demo").innerHTML = string
 }
 
 export default App;
